b5d0b1cb8e47cad7c82f2c01a0568e5b53064d80
AYAN KHAN <153628789+Ayankhann00@users.noreply.github.com>
2024-11-29 20:10:36 +0500

    Update Appwriteconfig.js

--- Appwriteconfig.js
+++ Appwriteconfig.js
@@ -5,7 +5,7 @@ export const client = new Client();
 
 client
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('6742315f001912d8f511'); // Replace with your project ID
+    .setProject(''); // Replace with your project ID
 
 export const account = new Account(client);
 export { ID } from 'appwrite';
